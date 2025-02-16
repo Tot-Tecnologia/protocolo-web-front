349ecd7f796f673d896f7a4a644b72fa61608830
Joao Azevedo <joaovarieira@gmail.com>
2025-02-15 23:26:28 -0300

    chore: configuracao inicial

---
+++ README.md
@@ -0,0 +1,17 @@
+# Front-end
+Repositório contendo o código do front-end do projeto.
+
+## Instalação
+Para fazer a instação do projeto, rode os comandos abaixo:
+```bash
+git clone TODO
+cd front-end
+npm install
+```
+
+## Uso
+Para iniciar o servidor de desenvolvimento:
+
+```bash
+npm run dev
+```
---
+++ eslint.config.js
@@ -0,0 +1,48 @@
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import prettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default tseslint.config(
+  { ignores: ["dist"] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      prettierRecommended,
+    ],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    settings: { react: { version: "19.0" } },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      react: react,
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
+      ...reactHooks.configs.recommended.rules,
+      "prettier/prettier": [
+        "warn",
+        {
+          // prettier rules
+        },
+      ],
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  },
+);
