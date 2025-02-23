7de71c268fa7f2097f0d559a211b2aa7236b6d04
Joao Azevedo <joaovarieira@gmail.com>
2025-02-23 17:48:39 -0300

    refactor: melhorando css do SignIn

--- src/views/SignIn/index.tsx
+++ src/views/SignIn/index.tsx
@@ -7,20 +7,23 @@ export function SignIn() {
   return (
     <MainPageWithImage title="Login">
       <form>
-        <Input className="mt-7.5 w-full" placeholder="CPF/CNPJ" />
-        <Input className="mt-4 w-full" placeholder="SENHA" />
-        <div className="flex justify-end">
-          <Link className="mt-4" href="#">
-            Recuperar senha
-          </Link>
-        </div>
+        <div className="flex flex-col gap-4">
+          <Input className="w-full" placeholder="CPF/CNPJ" />
+          <Input className="w-full" placeholder="SENHA" />
 
-        <Button className="mt-10 w-full" type="submit">
-          Acessar
-        </Button>
-        <Button className="mt-4 w-full" type="button" variant="outlined">
-          Cadastrar
-        </Button>
+          <div className="flex justify-end">
+            <Link className="mt-4" href="#">
+              Recuperar senha
+            </Link>
+          </div>
+
+          <Button className="mt-6 w-full" type="submit">
+            Acessar
+          </Button>
+          <Button className="w-full" type="button" variant="outlined">
+            Cadastrar
+          </Button>
+        </div>
       </form>
     </MainPageWithImage>
   );
