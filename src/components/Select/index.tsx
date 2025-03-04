04c6a0b224ee0bda128762ccee5bfd2968bd2150
Joao Azevedo <joaovarieira@gmail.com>
2025-03-04 20:09:29 -0300

    feat: criando componente Select

---
+++ src/components/Select/index.tsx
@@ -0,0 +1,25 @@
+import { BaseInput, IBaseInputProps } from "@/components/BaseInput";
+import clsx from "clsx";
+
+type ISelectProps = React.SelectHTMLAttributes<HTMLSelectElement> &
+  Omit<IBaseInputProps, "Component">;
+
+function SelectComponent(props: React.SelectHTMLAttributes<HTMLSelectElement>) {
+  return <select {...props} />;
+}
+
+export function Select(props: ISelectProps) {
+  return (
+    <BaseInput
+      Component={SelectComponent}
+      {...props}
+      className={clsx(
+        // setinha no fim do componente
+        "appearance-none bg-[url(/arrowDown.svg)] bg-[calc(100%_-_4_*_var(--spacing))_center] bg-no-repeat",
+        // outras estilizações
+        "text-sm",
+        props.className,
+      )}
+    />
+  );
+}
