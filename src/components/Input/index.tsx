c0a94fb429b1447917f5fabba34e90451ef8a1db
Joao Azevedo <joaovarieira@gmail.com>
2025-03-04 16:50:54 -0300

    refactor: tornando estilizacao do componente Input reaproveitavel

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,36 +1,12 @@
-import { useId } from "react";
-import { InputLabel } from "@/components/InputLabel";
-import clsx from "clsx";
+import { BaseInput, IBaseInputProps } from "@/components/BaseInput";
 
-interface IInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  label?: React.ReactNode;
-  containerClassName?: string;
+type IInputProps = React.InputHTMLAttributes<HTMLInputElement> &
+  Omit<IBaseInputProps, "Component">;
+
+function InputComponent(props: React.InputHTMLAttributes<HTMLInputElement>) {
+  return <input {...props} />;
 }
 
 export function Input(props: IInputProps) {
-  const isLabelVisible = props.label != null;
-
-  const generatedId = useId();
-
-  const id = props.id ?? generatedId;
-
-  return (
-    <div className={clsx("flex flex-col", props.containerClassName)}>
-      {isLabelVisible && (
-        <InputLabel htmlFor={id} className="self-start">
-          {props.label}
-        </InputLabel>
-      )}
-
-      <input
-        {...props}
-        id={id}
-        className={clsx(
-          "focus:border-primary focus:outline-primary rounded-md border-1 border-gray-300 px-3 py-1.5 text-gray-900 -outline-offset-2 focus:outline-2",
-          isLabelVisible && "mt-2",
-          props.className,
-        )}
-      />
-    </div>
-  );
+  return <BaseInput Component={InputComponent} {...props} />;
 }
