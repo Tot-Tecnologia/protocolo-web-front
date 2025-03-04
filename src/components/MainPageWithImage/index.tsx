4340e42ba118cd7141b36ea0a475f6f1bf25091a
Joao Azevedo <joaovarieira@gmail.com>
2025-03-04 16:50:01 -0300

    fix: corrigindo imagem com padding na pagina de login

--- src/components/MainPageWithImage/index.tsx
+++ src/components/MainPageWithImage/index.tsx
@@ -1,5 +1,3 @@
-import { PageContainer } from "@/components/PageContainer";
-
 type IMainPageWithImageProps = {
   title: React.ReactNode;
   children: React.ReactNode;
@@ -16,39 +14,37 @@ export function MainPageWithImage({
   fitImageToDisplayHeight,
 }: IMainPageWithImageProps) {
   return (
-    <PageContainer>
-      <div className="flex justify-between gap-4">
-        <div className="mx-auto max-w-90 basis-full md:basis-65/100">
-          <div className="mb-7.5 flex flex-col items-center gap-8">
-            <img
-              src="logo.png"
-              alt="Logo da prefeitura de Patrocínio"
-              className="h-18 md:h-24"
-            />
-
-            <h1 className="text-primary text-2xl font-bold">Protocolo Web</h1>
-
-            <h2 className="text-primary font-regular text-2xl uppercase">
-              {title}
-            </h2>
-          </div>
-
-          {children}
-        </div>
-
-        <div
-          className={
-            "hidden w-full basis-45/100 md:block" +
-            getImageHeight(fitImageToDisplayHeight)
-          }
-        >
+    <div className="flex justify-between gap-4">
+      <div className="mx-auto max-w-90 basis-full px-4 py-6 md:basis-65/100">
+        <div className="mb-7.5 flex flex-col items-center gap-8">
           <img
-            src="paisagem.png"
-            alt="Paisagem com uma igreja"
-            className="size-full object-cover"
+            src="logo.png"
+            alt="Logo da prefeitura de Patrocínio"
+            className="h-18 md:h-24"
           />
+
+          <h1 className="text-primary text-2xl font-bold">Protocolo Web</h1>
+
+          <h2 className="text-primary font-regular text-2xl uppercase">
+            {title}
+          </h2>
         </div>
+
+        {children}
       </div>
-    </PageContainer>
+
+      <div
+        className={
+          "hidden w-full basis-45/100 md:block" +
+          getImageHeight(fitImageToDisplayHeight)
+        }
+      >
+        <img
+          src="paisagem.png"
+          alt="Paisagem com uma igreja"
+          className="size-full object-cover"
+        />
+      </div>
+    </div>
   );
 }
