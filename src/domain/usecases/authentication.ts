f25c89a9cc545aeecd54d16ab0b11126315136c6
Joao Azevedo <joaovarieira@gmail.com>
2025-03-10 00:08:41 -0300

    feat: criando usecase de authentication com o metodo signin na camada de domain

---
+++ src/domain/usecases/authentication.ts
@@ -0,0 +1,10 @@
+import { AccountModel } from "@/domain/models";
+
+export type AuthenticationArgs = {
+  email: string;
+  password: string;
+};
+
+export interface Authentication {
+  signIn(args: AuthenticationArgs): Promise<AccountModel>;
+}
