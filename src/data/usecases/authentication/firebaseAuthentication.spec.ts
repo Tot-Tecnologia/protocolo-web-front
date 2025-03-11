c2c0e36a91593173ec7e073c4ecf1e883513c005
Joao Azevedo <joaovarieira@gmail.com>
2025-03-10 21:04:00 -0300

    test: fazendo signIn do FirebaseAuthentication retornar o statusCode e o accessToken corretos

--- src/data/usecases/authentication/firebaseAuthentication.spec.ts
+++ src/data/usecases/authentication/firebaseAuthentication.spec.ts
@@ -1,8 +1,12 @@
+import { HttpStatusCode } from "@/data/protocols/http/httpResponse";
 import { FirebaseAuthentication } from "@/data/usecases/authentication/firebaseAuthentication";
 import { mockAuthenticationArgs } from "@/domain/test/mockAuthenticationArgs";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { DeepPartial } from "@/types/utils";
+import { faker } from "@faker-js/faker";
+import { signInWithEmailAndPassword, UserCredential } from "firebase/auth";
 
 const auth = null as never;
+const accessToken = faker.string.uuid();
 
 const makeSut = () => {
   const sut = new FirebaseAuthentication(auth);
@@ -11,22 +15,37 @@ const makeSut = () => {
 
 vi.mock("firebase/auth", () => {
   return {
-    signInWithEmailAndPassword: vi.fn(),
+    signInWithEmailAndPassword: vi.fn(
+      (): DeepPartial<UserCredential> => ({
+        user: {
+          getIdToken: () => Promise.resolve(accessToken),
+        },
+      }),
+    ),
   };
 });
 
 describe("FirebaseAuthentication", () => {
-  test("should call firebase's signInWithEmailAndPassword with correct email and password", () => {
+  test("should call firebase's signInWithEmailAndPassword with correct email and password", async () => {
     const { sut } = makeSut();
 
     const authenticationArgs = mockAuthenticationArgs();
 
-    sut.signIn(authenticationArgs);
+    await sut.signIn(authenticationArgs);
 
     expect(signInWithEmailAndPassword).toHaveBeenCalledWith(
       auth,
       authenticationArgs.email,
       authenticationArgs.password,
     );
+  });
+
+  test("should return correct status code and accessToken on success", async () => {
+    const { sut } = makeSut();
+
+    const response = await sut.signIn(mockAuthenticationArgs());
+
+    expect(response.statusCode).toBe(HttpStatusCode.ok);
+    expect(response.body.accessToken).toBe(accessToken);
   });
 });
