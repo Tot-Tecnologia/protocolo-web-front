ef14d16d383a96e9b1fdfa715ca6c10e7670cbcb
Joao Azevedo <joaovarieira@gmail.com>
2025-03-12 22:30:38 -0300

    feat: acrescentando useForm ao componente de SignIn

--- src/presentation/views/SignIn/index.tsx
+++ src/presentation/views/SignIn/index.tsx
@@ -8,42 +8,49 @@ import {
   RECOVER_PASSWORD_ROUTE_URL,
   SIGN_UP_ROUTE_URL,
 } from "@/presentation/constants/routesUrl";
+import { FormProvider, useForm } from "react-hook-form";
 
 export function SignIn() {
+  const form = useForm();
+
+  const { handleSubmit } = form;
+
   const navigate = useNavigate();
 
-  const handleSignIn = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const handleSignIn = handleSubmit((data) => {
+    alert(JSON.stringify(data, null, 2));
     void navigate({ to: CREATE_PROCESSO_ROUTE_URL });
-  };
+  });
 
   const handleClickSignUp = () => navigate({ to: SIGN_UP_ROUTE_URL });
 
   return (
     <MainPageWithImage title="Login" fitImageToDisplayHeight>
-      <form onSubmit={handleSignIn}>
-        <div className="flex flex-col gap-4 *:w-full">
-          <Input placeholder="CPF/CNPJ" />
-          <Input placeholder="Senha" />
+      <FormProvider {...form}>
+        <form onSubmit={handleSignIn}>
+          <div className="flex flex-col gap-4 *:w-full">
+            <Input name="email" placeholder="E-mail" />
+            <Input name="password" placeholder="Senha" type="password" />
 
-          <div className="flex justify-end">
-            <Link to={RECOVER_PASSWORD_ROUTE_URL}>Recuperar senha</Link>
+            <div className="flex justify-end">
+              <Link to={RECOVER_PASSWORD_ROUTE_URL}>Recuperar senha</Link>
+            </div>
+
+            <Button className="mt-6" type="submit" size="large">
+              Acessar
+            </Button>
+
+            <Button
+              type="button"
+              variant="outlined"
+              size="large"
+              onClick={handleClickSignUp}
+            >
+              Cadastrar
+            </Button>
           </div>
-
-          <Button className="mt-6" type="submit" size="large">
-            Acessar
-          </Button>
-
-          <Button
-            type="button"
-            variant="outlined"
-            size="large"
-            onClick={handleClickSignUp}
-          >
-            Cadastrar
-          </Button>
-        </div>
-      </form>
+        </form>
+      </FormProvider>
     </MainPageWithImage>
   );
 }
