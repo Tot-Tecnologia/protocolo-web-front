d89ea338cea961ebd8395fce6e1d3dfd4cf15af5
Joao Azevedo <joaovarieira@gmail.com>
2025-03-13 22:59:12 -0300

    feat: adicionando useForm ao componente SignUp

--- src/presentation/views/SignUp/index.tsx
+++ src/presentation/views/SignUp/index.tsx
@@ -3,38 +3,43 @@ import { Button } from "@/presentation/components/Button";
 import { Input } from "@/presentation/components/Input";
 import { MainPageWithImage } from "@/presentation/components/MainPageWithImage";
 import { SIGN_IN_ROUTE_URL } from "@/presentation/constants/routesUrl";
+import { FormProvider, useForm } from "react-hook-form";
 
 export function SignUp() {
+  const form = useForm();
   const navigate = useNavigate();
+
+  const { handleSubmit } = form;
+
+  const handleSignUp = handleSubmit((data) => {
+    alert(JSON.stringify(data, null, 2));
+  });
 
   const handleClickBack = () => navigate({ to: SIGN_IN_ROUTE_URL });
 
   return (
     <MainPageWithImage title="Cadastrar">
-      <form>
-        <div className="flex flex-col gap-4 *:w-full">
-          <Input placeholder="CPF/CNPJ" />
-          <Input placeholder="Nome" />
-          <Input placeholder="Celular" />
-          <Input placeholder="E-mail" />
-          <Input placeholder="Confirmar e-mail" />
-          <Input placeholder="Senha" />
-          <Input placeholder="Confirmar senha" />
+      <FormProvider {...form}>
+        <form onSubmit={handleSignUp}>
+          <div className="flex flex-col gap-4 *:w-full">
+            <Input name="cpfCnpj" placeholder="CPF/CNPJ" />
+            <Input name="nome" placeholder="Nome" />
+            <Input name="telefone" placeholder="Celular" />
+            <Input name="email" placeholder="E-mail" />
+            <Input name="confirmacaoEmail" placeholder="Confirmar e-mail" />
+            <Input name="senha" placeholder="Senha" />
+            <Input name="confirmacaoSenha" placeholder="Confirmar senha" />
 
-          <Button className="mt-6" type="submit" size="large">
-            Cadastrar
-          </Button>
+            <Button className="mt-6" type="submit" size="large">
+              Cadastrar
+            </Button>
 
-          <Button
-            type="submit"
-            variant="outlined"
-            size="large"
-            onClick={handleClickBack}
-          >
-            Voltar
-          </Button>
-        </div>
-      </form>
+            <Button variant="outlined" size="large" onClick={handleClickBack}>
+              Voltar
+            </Button>
+          </div>
+        </form>
+      </FormProvider>
     </MainPageWithImage>
   );
 }
