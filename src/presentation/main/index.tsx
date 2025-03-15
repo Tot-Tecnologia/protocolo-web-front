6d02a4643ef15621affd3f6bfb43a66818851dba
Joao Azevedo <joaovarieira@gmail.com>
2025-03-15 10:48:04 -0300

    chore: instalando lib tanstack-query (antigo react-query)

--- src/presentation/main/index.tsx
+++ src/presentation/main/index.tsx
@@ -1,13 +1,19 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "@tanstack/react-router";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 // import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { queryClient } from "@/presentation/queryClient";
 import { router } from "@/presentation/router";
 import "@/presentation/main/theme.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
-    {/* <TanStackRouterDevtools router={router} /> */}
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      {/* <TanStackRouterDevtools router={router} /> */}
+      {/* <ReactQueryDevtools client={queryClient} /> */}
+    </QueryClientProvider>
   </StrictMode>,
 );
--- src/presentation/router/index.ts
+++ src/presentation/router/index.ts
@@ -1,4 +1,10 @@
+import { queryClient } from "@/presentation/queryClient";
 import { routeTree } from "@/presentation/router/generated/routeTree.gen";
 import { createRouter } from "@tanstack/react-router";
 
-export const router = createRouter({ routeTree });
+export const router = createRouter({
+  routeTree,
+  context: { queryClient },
+  defaultPreload: "intent",
+  defaultPreloadStaleTime: 0,
+});
