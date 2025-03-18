13650f3b3b2f0c73e532c83622f0a0d1367e400a
Joao Azevedo <joaovarieira@gmail.com>
2025-03-17 22:02:13 -0300

    feat: criando regExps globais de CPF e CNPJ

---
+++ src/presentation/constants/regExps.ts
@@ -0,0 +1,6 @@
+export const CPF_REGEXP = /(\d{3}.\d{3}.\d{3}-\d{2})/;
+
+export const CNPJ_REGEXP = /\d{2}.\d{3}.\d{3}\/\d{4}-\d{2}/;
+
+export const CPF_CNPJ_REGEXP =
+  /(\d{3}.\d{3}.\d{3}-\d{2})|(\d{2}.\d{3}.\d{3}\/\d{4}-\d{2})/;
