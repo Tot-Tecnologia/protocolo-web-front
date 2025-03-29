4957044d0e75126d533f95ce2759b868b8abaa7a
Joao Azevedo <joaovarieira@gmail.com>
2025-03-29 19:51:40 -0300

    feat: criando hook useLocalStorage para abstrair a api localStorage

---
+++ src/presentation/hooks/useLocalStorage.ts
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+type IUseLocalStorage<T> = [
+  storedValue: T,
+  setValue: (value: T | ((value: T) => T)) => void,
+];
+
+/**
+ * Abstração da API Window.localStorage em um hook.
+ *
+ * @see https://developer.mozilla.org/pt-BR/docs/Web/API/Window/localStorage
+ */
+export function useLocalStorage<T>(
+  key: string,
+  initialValue: T,
+): IUseLocalStorage<T> {
+  const getLocalStorageKey = () => {
+    return `@ProtocoloWeb__Key=${key}`;
+  };
+
+  const [storedValue, setStoredValue] = useState(() => {
+    if (typeof window === "undefined") {
+      return initialValue;
+    }
+    try {
+      const item = window.localStorage.getItem(getLocalStorageKey());
+      return item ? (JSON.parse(item) as T) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value: T | ((value: T) => T)) => {
+    try {
+      // Permite que o parâmetro "value" seja uma função,
+      // imitando o comportamento do useState.
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+
+      setStoredValue(valueToStore);
+
+      if (typeof window !== "undefined") {
+        window.localStorage.setItem(
+          getLocalStorageKey(),
+          JSON.stringify(valueToStore),
+        );
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue];
+}
