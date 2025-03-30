aa0fd6eac0c80e57bc5d632c1a85098d57131142
Joao Azevedo <joaovarieira@gmail.com>
2025-03-29 21:22:11 -0300

    refactor: forçando tipagem do retorno de mockAuthenticationArgs

--- src/domain/test/mockAuthenticationArgs.ts
+++ src/domain/test/mockAuthenticationArgs.ts
@@ -1,6 +1,7 @@
+import { AuthenticationArgs } from "@/domain/usecases";
 import { faker } from "@faker-js/faker";
 
-export function mockAuthenticationArgs() {
+export function mockAuthenticationArgs(): AuthenticationArgs {
   const email = faker.internet.email();
   const password = faker.internet.password();
 
