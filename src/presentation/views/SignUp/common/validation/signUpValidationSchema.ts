1d7ea664f53630bcf6ff32e609653fba5f52991e
Joao Azevedo <joaovarieira@gmail.com>
2025-03-30 17:11:22 -0300

    feat: refinando o signUpValidationSchema para remover os caracteres nao numericos do cpfCnpj e telefone

--- src/presentation/views/SignUp/common/validation/signUpValidationSchema.ts
+++ src/presentation/views/SignUp/common/validation/signUpValidationSchema.ts
@@ -2,18 +2,25 @@ import { AddAccountArgs } from "@/domain/usecases/addAccount";
 import { CPF_CNPJ_REGEXP } from "@/presentation/constants/regExps";
 import { z } from "zod";
 
-export type SignUpDto = AddAccountArgs;
+export type SignUpDto = AddAccountArgs & {
+  confirmacaoEmail: string;
+  confirmacaoSenha: string;
+};
 
 export const signUpValidationSchema: z.Schema<SignUpDto> = z
   .object({
-    cpfCnpj: z.string().regex(CPF_CNPJ_REGEXP),
+    cpfCnpj: z
+      .string()
+      .regex(CPF_CNPJ_REGEXP)
+      .transform((input) => input.replace(/\D/g, "")),
     nome: z.string().min(1, "Obrigatório"),
     telefone: z
       .string()
-      .regex(/^\d{2}\d{8,9}$/, "Deve conter DDD e 8 ou 9 dígitos"),
+      .regex(/^\d{2}\d{8,9}$/, "Deve conter DDD e 8 ou 9 dígitos")
+      .transform((input) => input.replace(/\D/g, "")),
     email: z.string().email(),
     confirmacaoEmail: z.string().email(),
-    senha: z.string(),
+    senha: z.string().min(6),
     confirmacaoSenha: z.string(),
   })
   .refine((data) => data.email === data.confirmacaoEmail, {
