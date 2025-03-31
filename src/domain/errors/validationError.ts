c78b548279e7c0b8e2ab7065121bfc1cba9ddff3
Joao Azevedo <joaovarieira@gmail.com>
2025-03-31 20:08:23 -0300

    feat: adicionando mensagem de erro padrao para o ValidationException quando o array de erros estiver vazio

--- src/domain/errors/validationError.ts
+++ src/domain/errors/validationError.ts
@@ -6,8 +6,14 @@ export class ValidationError extends Error {
   errors: string[];
 
   constructor({ errors = [] }: IValidationErrorArgs = {}) {
-    super(errors.join("; "));
-    this.errors = errors;
+    if (errors.length) {
+      super(errors.join("; "));
+      this.errors = errors;
+    } else {
+      const unexpectedMessage = "Erro de validação desconhecido.";
+      super(unexpectedMessage);
+      this.errors = [unexpectedMessage];
+    }
     this.name = "ValidationError";
   }
 }
