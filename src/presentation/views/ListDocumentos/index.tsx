09477a8740032649a1cde97b0938530f284d2301
Joao Azevedo <joaovarieira@gmail.com>
2025-03-31 23:16:26 -0300

    feat: adicionando componentes de filtro na tela de Consultar Solicitacao

--- src/presentation/views/ListDocumentos/index.tsx
+++ src/presentation/views/ListDocumentos/index.tsx
@@ -1,5 +1,40 @@
+import { Button } from "@/presentation/components/Button";
+import { Input } from "@/presentation/components/Input";
 import { PageContainer } from "@/presentation/components/PageContainer";
+import { Select } from "@/presentation/components/Select";
+import { useFormWithZod } from "@/presentation/hooks/useFormWithZod";
+import { FormProvider } from "react-hook-form";
+import { z } from "zod";
 
 export function ListDocumentos() {
-  return <PageContainer title="Consultar Solicitação">TODO</PageContainer>;
+  const form = useFormWithZod({ schema: z.object({}) });
+
+  return (
+    <PageContainer title="Consultar Solicitação">
+      <FormProvider {...form}>
+        <form
+          onSubmit={() => alert("// TODO")}
+          className="grid items-end gap-4 md:grid-cols-3 lg:grid-cols-[minmax(0,1fr)_minmax(0,1fr)_minmax(0,1fr)_auto_auto]"
+        >
+          <Input name="numeroDocumento" label="Número Documento" />
+
+          <Input name="ano" label="Ano" />
+
+          <Select name="tipoSolicitacao" label="Tipo de Documento">
+            <option value=""></option>
+            <option value="lorem">Lorem</option>
+            <option value="ipsum">Ipsum</option>
+          </Select>
+
+          <span className="hidden text-[0px] md:block lg:hidden">
+            Essa span só serve para ocupar uma coluna do grid em telas md
+          </span>
+
+          <Button className="mt-4 md:mt-0">Consultar</Button>
+
+          <Button variant="outlined">Limpar</Button>
+        </form>
+      </FormProvider>
+    </PageContainer>
+  );
 }
