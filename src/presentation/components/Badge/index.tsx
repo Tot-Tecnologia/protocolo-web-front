d3f2cb5f0982f26b1db565a479a337ae49b7c5f4
Joao Azevedo <joaovarieira@gmail.com>
2025-04-10 22:34:28 -0300

    feat: criando componente Badge

---
+++ src/presentation/components/Badge/index.tsx
@@ -0,0 +1,29 @@
+import { ThemeColor } from "@/types/utils";
+import clsx from "clsx";
+
+type IBadgeProps = {
+  children: React.ReactNode;
+  color?: ThemeColor;
+};
+
+const colorVariants: Record<ThemeColor, string> = {
+  error: "text-error-dark bg-rose-50",
+  info: "text-info-dark bg-indigo-50",
+  primary: "text-primary bg-indigo-50",
+  secondary: "text-secondary bg-stone-300",
+  success: "text-success-dark bg-emerald-50",
+  warning: "text-warning-dark bg-amber-50",
+};
+
+export function Badge({ color = "info", children }: IBadgeProps) {
+  return (
+    <span
+      className={clsx(
+        "inline-flex items-center justify-center rounded-full px-2 py-0.5",
+        colorVariants[color],
+      )}
+    >
+      <p className="text-sm whitespace-nowrap">{children}</p>
+    </span>
+  );
+}
