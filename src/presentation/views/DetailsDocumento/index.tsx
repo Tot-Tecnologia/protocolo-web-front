00e5615f4a6638a0db508b8f642113d77eb28b30
Joao Azevedo <joaovarieira@gmail.com>
2025-04-18 14:47:47 -0300

    feat: criando mock do card Informacoes do componente DetailsDocumento

--- src/presentation/views/DetailsDocumento/index.tsx
+++ src/presentation/views/DetailsDocumento/index.tsx
@@ -1,3 +1,4 @@
+import { Card } from "@/presentation/components/Card";
 import { PageContainer } from "@/presentation/components/PageContainer";
 import { LIST_DOCUMENTOS_ROUTE_URL } from "@/presentation/constants/routesUrl";
 
@@ -7,7 +8,32 @@ export function DetailsDocumento() {
       navigateBackwardTo={LIST_DOCUMENTOS_ROUTE_URL}
       title="Consultar solicitação"
     >
-      #TODO
+      <Card title="Informações">
+        <div className="flex flex-col gap-y-4">
+          <div className="flex flex-col gap-x-10 gap-y-4 md:flex-row">
+            <p>
+              <b>Número</b>: 0138
+            </p>
+
+            <p>
+              <b>Situação</b>: Aguardando pagamento de guia
+            </p>
+          </div>
+
+          <p>
+            <b>Tipo</b>: Alteração cadastral
+          </p>
+
+          <p>
+            <b>Órgão responsável</b>: Secretaria de Finanças
+          </p>
+
+          <p>
+            <b>Observação</b>: Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Culpa facilis, adipisci nam a eveniet illum.
+          </p>
+        </div>
+      </Card>
     </PageContainer>
   );
 }
