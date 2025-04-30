d4068a0593a12db5ad132b278cc4ed6d3f613847
Joao Azevedo <joaovarieira@gmail.com>
2025-04-29 22:18:47 -0300

    feat: integrando parcialmente os campos do filtro de protocolos ao react-hook-form, falta o campo Tipo de solicitacao

--- src/presentation/views/ListProtocolos/common/components/ListProtocolosFilter/index.tsx
+++ src/presentation/views/ListProtocolos/common/components/ListProtocolosFilter/index.tsx
@@ -1,24 +1,46 @@
 import { FormProvider } from "react-hook-form";
-import { z } from "zod";
 import { Button } from "@/presentation/components/Button";
 import { Input } from "@/presentation/components/Input";
 import { Select } from "@/presentation/components/Select";
 import { useFormWithZod } from "@/presentation/hooks/useFormWithZod";
+import { onlyDigitsHandler } from "@/presentation/utils/inputMasks";
+import {
+  listProtocolosFilterDefaultValues,
+  ListProtocolosFilterDto,
+  listProtocolosFilterValidationSchema,
+} from "../../validations/listProtocolosFilterValidationSchema";
 
 export function ListProtocolosFilter() {
-  const form = useFormWithZod({ schema: z.object({}) });
+  const form = useFormWithZod({
+    schema: listProtocolosFilterValidationSchema,
+    defaultValues: listProtocolosFilterDefaultValues,
+  });
+
+  // TODO: adicionar uma maneira de obter a página selecionada atualmente
+  const handleSubmitForm = form.handleSubmit(
+    (data) => alert(JSON.stringify(data, null, 2)), // TODO: tratar sucesso
+    (errors) => alert(JSON.stringify(errors, null, 2)), // TODO: tratar erro
+  );
 
   return (
     <FormProvider {...form}>
       <form
-        onSubmit={() => alert("// TODO")}
+        onSubmit={handleSubmitForm}
         className="grid items-end gap-4 md:grid-cols-3 lg:grid-cols-[minmax(0,1fr)_minmax(0,1fr)_minmax(0,1fr)_auto_auto]"
       >
-        <Input name="numeroProtocolo" label="Número Protocolo" />
+        <Input<ListProtocolosFilterDto>
+          name="numeroProtocolo"
+          label="Número Protocolo"
+          onChange={onlyDigitsHandler}
+        />
 
-        <Input name="ano" label="Ano" />
+        <Input<ListProtocolosFilterDto>
+          name="ano"
+          label="Ano"
+          onChange={onlyDigitsHandler}
+        />
 
-        <Select name="tipoSolicitacao" label="Tipo de Protocolo">
+        <Select name="tipoSolicitacao" label="Tipo de solicitação">
           <option value=""></option>
           <option value="lorem">Lorem</option>
           <option value="ipsum">Ipsum</option>
@@ -28,7 +50,9 @@ export function ListProtocolosFilter() {
           Essa span só serve para ocupar uma coluna do grid em telas md
         </span>
 
-        <Button className="mt-4 md:mt-0">Consultar</Button>
+        <Button className="mt-4 md:mt-0" type="submit">
+          Consultar
+        </Button>
 
         <Button variant="outlined">Limpar</Button>
       </form>
