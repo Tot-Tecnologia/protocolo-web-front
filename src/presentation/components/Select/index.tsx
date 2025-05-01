55b81a581502be69de708c74ba51a4737a24f852
Joao Azevedo <joaovarieira@gmail.com>
2025-05-01 17:52:50 -0300

    fix: removendo cursor-pointer do Select quando este estah desabilitado

--- src/presentation/components/Select/index.tsx
+++ src/presentation/components/Select/index.tsx
@@ -40,7 +40,8 @@ export function Select(props: ISelectProps) {
             // setinha no fim do componente
             "appearance-none bg-[url(/arrowDownIcon.svg)] bg-[calc(100%_-_4_*_var(--spacing))_center] bg-no-repeat",
             // outras estilizações
-            "cursor-pointer text-sm leading-6",
+            "text-sm leading-6",
+            props.disabled ? "cursor-default" : "cursor-pointer",
             props.className,
           )}
         />
