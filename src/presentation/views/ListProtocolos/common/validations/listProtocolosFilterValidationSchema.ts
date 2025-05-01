ae1906c5e96eb143d80d69fb0fa56348c15740e8
Joao Azevedo <joaovarieira@gmail.com>
2025-05-01 17:55:15 -0300

    refactor: removendo propriedade nao utilizada cpfCnpj do listProtocolosFilterValidationSchema

--- src/presentation/views/ListProtocolos/common/validations/listProtocolosFilterValidationSchema.ts
+++ src/presentation/views/ListProtocolos/common/validations/listProtocolosFilterValidationSchema.ts
@@ -5,14 +5,12 @@ export type ListProtocolosFilterDto = Partial<ILoadProtocoloListArgs>;
 
 export const listProtocolosFilterValidationSchema: z.Schema<ListProtocolosFilterDto> =
   z.object({
-    cpfCnpj: z.string().nullish(),
     numeroProtocolo: z.coerce.number().nullish(),
     ano: z.coerce.number().nullish(),
     tipoSolicitacao: z.coerce.number().nullish(),
   });
 
 export const listProtocolosFilterDefaultValues: ListProtocolosFilterDto = {
-  cpfCnpj: "",
   ano: null,
   numeroProtocolo: null,
   tipoSolicitacao: null,
