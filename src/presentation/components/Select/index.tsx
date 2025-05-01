b17402ac837df26f727e53a58ec56fad9ccbe871
Joao Azevedo <joaovarieira@gmail.com>
2025-05-01 13:14:16 -0300

    feat: integrando componente Select ao controller da lib react-hook-form

--- src/presentation/components/Select/index.tsx
+++ src/presentation/components/Select/index.tsx
@@ -3,12 +3,16 @@ import {
   IBaseInputProps,
 } from "@/presentation/components/BaseInput";
 import clsx from "clsx";
+import { Controller, FieldValues, Path } from "react-hook-form";
 
-type ISelectProps = React.SelectHTMLAttributes<HTMLSelectElement> &
-  Omit<
-    IBaseInputProps<React.SelectHTMLAttributes<HTMLSelectElement>>,
-    "Component"
-  >;
+export type ISelectProps<TFieldValues extends FieldValues = FieldValues> =
+  React.SelectHTMLAttributes<HTMLSelectElement> &
+    Omit<
+      IBaseInputProps<React.SelectHTMLAttributes<HTMLSelectElement>>,
+      "Component"
+    > & {
+      name: Path<TFieldValues>;
+    };
 
 function SelectComponent(props: React.SelectHTMLAttributes<HTMLSelectElement>) {
   return <select {...props} />;
@@ -16,15 +20,30 @@ function SelectComponent(props: React.SelectHTMLAttributes<HTMLSelectElement>) {
 
 export function Select(props: ISelectProps) {
   return (
-    <BaseInput<React.SelectHTMLAttributes<HTMLSelectElement>>
-      Component={SelectComponent}
-      {...props}
-      className={clsx(
-        // setinha no fim do componente
-        "appearance-none bg-[url(/arrowDownIcon.svg)] bg-[calc(100%_-_4_*_var(--spacing))_center] bg-no-repeat",
-        // outras estilizações
-        "cursor-pointer text-sm leading-6",
-        props.className,
+    <Controller
+      name={props.name}
+      render={({ field, fieldState }) => (
+        <BaseInput<React.SelectHTMLAttributes<HTMLSelectElement>>
+          Component={SelectComponent}
+          {...props}
+          {...field}
+          value={field.value as never}
+          helperText={fieldState.error?.message ?? props.helperText}
+          error={!!fieldState.error || props.error}
+          onChange={(event) => {
+            if (props.onChange) {
+              props.onChange(event);
+            }
+            field.onChange(event);
+          }}
+          className={clsx(
+            // setinha no fim do componente
+            "appearance-none bg-[url(/arrowDownIcon.svg)] bg-[calc(100%_-_4_*_var(--spacing))_center] bg-no-repeat",
+            // outras estilizações
+            "cursor-pointer text-sm leading-6",
+            props.className,
+          )}
+        />
       )}
     />
   );
