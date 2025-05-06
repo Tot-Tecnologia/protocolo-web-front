6964a629a5a2ee702795d27c03d9f4e4f016989a
Guilherme <cicllostecnologialtda@MacBook-Air-de-Cicllos.local>
2025-05-06 18:19:26 -0300

    retirada do path /cidadao

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/cidadao/",
   test: {
     globals: true,
     environment: "jsdom",
