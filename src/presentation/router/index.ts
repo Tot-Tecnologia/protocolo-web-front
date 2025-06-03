f1a1ad810e260d446a7b229c30f1fb486866472c
Eliezer <silva.eliezertadeu@gmail.com>
2025-06-03 20:41:36 -0300

    feat: hash router test

--- src/presentation/router/index.ts
+++ src/presentation/router/index.ts
@@ -1,10 +1,11 @@
 import { queryClient } from "@/presentation/queryClient";
 import { routeTree } from "@/presentation/router/generated/routeTree.gen";
-import { createRouter } from "@tanstack/react-router";
+import { createHashHistory, createRouter } from "@tanstack/react-router";
 
 export const router = createRouter({
   routeTree,
   context: { queryClient },
   defaultPreload: "intent",
   defaultPreloadStaleTime: 0,
+  history: createHashHistory(),
 });
