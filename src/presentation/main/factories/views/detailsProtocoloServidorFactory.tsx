b91e2fb12d90d4417ae4daa8f076da8aba8c2c58
Eliezer <silva.eliezertadeu@gmail.com>
2025-06-04 15:21:52 -0300

    feat: ajuste na factory

--- src/presentation/main/factories/views/detailsProtocoloServidorFactory.tsx
+++ src/presentation/main/factories/views/detailsProtocoloServidorFactory.tsx
@@ -1,10 +1,12 @@
 import { makeAddDocumentosToProtocolo } from "@/presentation/main/factories/usecases/addDocumentosToProtocoloFactory";
 import { makeLoadProtocoloDetails } from "@/presentation/main/factories/usecases/loadProtocoloDetailsFactory";
+import { makeUiNotification } from "@/presentation/main/factories/usecases/uiNotificationFactory";
 import { DetailsProtocoloServidor } from "@/presentation/views/DetailsProtocoloServidor";
 
 export function makeDetailsProtocoloServidor() {
 	return (
 		<DetailsProtocoloServidor
+			uiNotification={makeUiNotification()}
 			loadProtocoloDetails={makeLoadProtocoloDetails()}
 			addDocumentosToProtocolo={makeAddDocumentosToProtocolo({ tipo: 'guias' })}
 		/>
